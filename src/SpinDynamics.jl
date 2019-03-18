9f1f9dec193785e255e8b9048aec51f5cdf24937
Weiwei Wang <wangweiwei@ahu.edu.cn>
2019-03-18 16:59:38 +0800

    continue to run the simulation if gpu is not available

--- src/SpinDynamics.jl
+++ src/SpinDynamics.jl
@@ -4,7 +4,6 @@ using Printf
 export create_mesh, create_sim,
        init_m0, add_zeeman, add_dmi,
        add_exch, add_anis, add_demag,
-       add_demag_gpu, 
        run_until, relax, save_vtk
 
 include("head.jl")
@@ -18,7 +17,13 @@ include("ode.jl")
 include("fileio.jl")
 include("sim.jl")
 include("demag.jl")
-include("demag_gpu.jl")
 
 include("vtk.jl")
+
+try
+	using CUDAdrv
+	include("demag_gpu.jl")
+catch
+    @warn "CUDA is not available!"
+end
 end
