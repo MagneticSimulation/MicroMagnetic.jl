e81710ff4a55d92e37ef40d7659e15c68ff79798
Weiwei Wang <wangweiwei@ahu.edu.cn>
2019-04-10 21:34:37 +0800

    fix cuda test due to silly copy

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,8 +6,8 @@ include("test_demag.jl")
 include("test_interface.jl")
 
 if JuMag._cuda_available.x
-  include("test_sim.jl")
-  include("test_exch.jl")
-  include("test_demag.jl")
-  include("test_llg.jl")
+  include("cuda/test_sim.jl")
+  include("cuda/test_exch.jl")
+  include("cuda/test_demag.jl")
+  include("cuda/test_llg.jl")
 end
