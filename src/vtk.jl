5c53aa606418abb526f1378e101ccab727857fbf
Weiwei Wang <wangweiwei@ahu.edu.cn>
2019-10-17 23:16:01 +0800

    add save_vtk_points

--- src/vtk.jl
+++ src/vtk.jl
@@ -66,6 +66,37 @@ function save_vtk(sim::AbstractSim, fname::String; fields::Array{String, 1} = St
   vtk_save(vtk)
 end
 
+function save_vtk_points(sim::AbstractSim, fname::String; fields::Array{String, 1} = String[])
+  mesh = sim.mesh
+  nx, ny, nz = mesh.nx, mesh.ny, mesh.nz
+  xyz = zeros(Float32, 3, nx, ny, nz)
+  dx, dy, dz = 1.0,1.0,1.0
+  if isa(mesh, CubicMesh)
+    dx, dy, dz=mesh.a, mesh.a, mesh.a
+  else
+    dx, dy, dz=mesh.dx, mesh.dy, mesh.dz
+  end
+  for k = 1:nz, j = 1:ny, i = 1:nx
+    xyz[1, i, j, k] = (i-0.5)*dx
+    xyz[2, i, j, k] = (j-0.5)*dy
+    xyz[3, i, j, k] = (k-0.5)*dz
+  end
+  vtk = vtk_grid(fname, xyz)
+  b = reshape(sim.spin, (3, nx, ny, nz))
+  vtk_point_data(vtk, b , "m")
+
+  if length(fields) > 0
+    fields = Set(fields)
+    for i in sim.interactions
+      if i.name in fields
+        b = reshape(i.field, (3, nx, ny, nz))
+        vtk_point_data(vtk, b, i.name)
+      end
+    end
+  end
+  vtk_save(vtk)
+end
+
 
 function ovf2vtk(mesh, ovf_name, vtk_name)
     sim = Sim(mesh, driver="SD")
