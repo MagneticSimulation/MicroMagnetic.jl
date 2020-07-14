5eab4f84678f6dd15d2f031e63ffc75033583854
magnet <1424595185@qq.com>
2020-07-14 19:58:26 +0800

    add function Make_Projection

--- src/tools.jl
+++ src/tools.jl
@@ -105,8 +105,12 @@ function fft_m(spin, nx, ny, nz, dx, dy, dz, axis, zero_padding_size=-1)
         end
         return kx, ky, Intensity./nz
     elseif c == 2
-        println("will be added later!")
-        return nothing
+        Intensity = zeros(Nx, Nz)
+        for j = 1:ny
+            kx, kz, I = fft_m_2d(m[2,:,j,:], dx, dz, zero_padding_size)
+            Intensity .+= I
+        end
+        return kx, kz, Intensity./ny
     elseif c == 1
         Intensity = zeros(Ny, Nz)
         for i = 1:nx
@@ -147,7 +151,7 @@ function compute_electric_phase(V, V0, dz, nz, beta)
     return lambda, phi_E
 end
 
-function compute_magnetic_phase_direct(mx, my, dx, dy, Lz, Ms, i0, j0)  #slow, for testing purpose
+function compute_magnetic_phase_direct(mx, my, dx, dy, dz, Ms, i0, j0)  #slow, for testing purpose
     Nx, Ny = size(mx)
     mu0 = 4*pi*1e-7
     Phi0 = 2.067833e-15
@@ -161,11 +165,11 @@ function compute_magnetic_phase_direct(mx, my, dx, dy, Lz, Ms, i0, j0)  #slow, f
             phi += (y*mx[i,j] -  x*my[i,j])/r^2
         end
     end
-    return -phi*mu0*Ms*Lz/(2*Phi0)*dx*dy
+    return -phi*mu0*Ms*dz/(2*Phi0)*dx*dy
 end
 
 
-function compute_magnetic_phase_fft(mx, my, dx, dy, Lz, Ms; Nx=-1, Ny=-1)
+function compute_magnetic_phase_fft(mx, my, dx, dy, dz, Ms; Nx=-1, Ny=-1)
     mu0 = 4*pi*1e-7
     Phi0 = 2.067833e-15
     nx, ny =  size(mx)
@@ -208,7 +212,7 @@ function compute_magnetic_phase_fft(mx, my, dx, dy, Lz, Ms; Nx=-1, Ny=-1)
         phi[i,j] = phi_all[i+nx-1, j+ny-1]
     end
 
-    return -phi*mu0*Ms*Lz/(2*Phi0)*dx*dy
+    return -phi*mu0*Ms*dz/(2*Phi0)*dx*dy
 end
 
 #Ref: "MALTS: A tool to simulate Lorentz Transmission Electron Microscopy from micromagnetic simulations" by Stephanie K. Walton
@@ -217,9 +221,9 @@ end
 #V0 is the mean inner potential (MIP)
 #alpha: beam divergence angle
 #axis="x" is the normal direction
-#beta is the angle between electron beam and the normal axis, in radian
-function LTEM(ovf_name; V=300, Ms=1e5, V0=-26, df=1600, alpha=1e-5, zero_padding_size=512,axis="z",beta=0)
-    ovf = read_ovf(ovf_name)
+#beta is the angle between electron beam and the normal  axis, in radian
+function LTEM(fname; V=300, Ms=1e5, V0=-26, df=1600, alpha=1e-5, Nx=512, Ny=512,beta=0,gamma=0,ItpNum=0)
+    ovf = read_ovf(fname)
     nx = ovf.xnodes
     ny = ovf.ynodes
     nz = ovf.znodes
@@ -230,34 +234,17 @@ function LTEM(ovf_name; V=300, Ms=1e5, V0=-26, df=1600, alpha=1e-5, zero_padding
     spin = ovf.data
     m = reshape(spin,(3, nx, ny, nz))
 
-    mx1,my1,mz1 = m_average(ovf,axis=axis)
-    mx = mx1
-    my = cos(beta)*my1-sin(beta)*mz1
-    if axis == "x"
-        nx,dx=ny,dy
-        ny,dy=nz,dz
-    elseif axis == "y"
-        ny,dy=nz,dz
-    end
+    sum_mx,sum_my,sum_mz = Make_Projection(ovf,beta=beta,gamma=gamma,Nx=Nx,Ny=Ny,ItpNum=ItpNum)
+    smx,smy,smz = sum_mx/nz,sum_my/nz,sum_mz/nz
 
-    dx=dx*cos(beta)
+    #dx=dx*cos(beta)
 
     lambda, phi_E = compute_electric_phase(1000*V, V0, dz, nz, beta)
 
-    N = zero_padding_size
-    Nx = N > nx ? N : nx
-    Ny = N > ny ? N : ny
-
     #put data on the center of zero padding square
-    new_mx = zeros(Nx, Ny)
-    new_my = zeros(Nx, Ny)
-    for i=1:nx, j=1:ny
-        new_mx[Int(floor(Nx/2-nx/2+i)),Int(floor(Ny/2-ny/2+j))] = mx[i,j]
-        new_my[Int(floor(Nx/2-nx/2+i)),Int(floor(Ny/2-ny/2+j))] = my[i,j]
-    end
 
-    fft_mx = fft(new_mx)
-    fft_my = fft(new_my)
+    fft_mx = fft(smx)
+    fft_my = fft(smy)
 
     kx = fftfreq(Nx, d=dx)
     ky = fftfreq(Ny, d=dy)
@@ -285,7 +272,7 @@ function LTEM(ovf_name; V=300, Ms=1e5, V0=-26, df=1600, alpha=1e-5, zero_padding
     end
     Phi_M[1,1]=0
 
-    phi_M = 1/cos(beta)*real.(ifft(Phi_M))
+    phi_M = real.(ifft(Phi_M))
 
     phi = phi_M
 
@@ -307,51 +294,129 @@ function LTEM(ovf_name; V=300, Ms=1e5, V0=-26, df=1600, alpha=1e-5, zero_padding
     end
 
     return local_phi, local_intensity
+    #return local_phi, intensity
 end
 
-function m_average(m::Array{T,1},nx::Int,ny::Int,nz::Int;axis::String="z") where T<:AbstractFloat ##axis can only chosen from "x" "y" "z"
+"""Make_Projection: get the projection magnetization.
+nx ny nz is the shape of m, Nx Ny is the projection size
 
-    if length(m) != 3*nx*ny*nz
-        println("Length doesn't match!")
-        return nothing
-    end
+First step: rotate beta(rad) with axis (0,-1,0)
+Second step : rotate gamma(rad) with axis (1,0,0)
+positive degree for anti-clockwise,negative for clockwise
+
+ItpNum:Interpolation within an unit length
+for example:ItpNum=2,insert 3*3*3-8 unit cells from [i,j,k] to [i+1,j+1,k+1] for every cell
+Using ItpNum can improve the accuracy of projection operation at a cost of calculation speed
+(not suggested in ordinary)
+
+```julia
+Make_Projection(m,nx,ny,nz,Nx=128,Ny=128,beta=pi/3)
+```
+"""
+function Make_Projection(m::Array{T,1},nx,ny,nz; Nx::Int=-1,Ny::Int=-1,
+                        beta::Number=0,gamma::Number=0,ItpNum::Int=0) where T<:AbstractFloat
+
+    Nx = Nx > 0 ? Nx : nx
+    Ny = Ny > 0 ? Ny : ny
 
     b = reshape(m,(3,nx,ny,nz))
-    N = 0
-    if axis =="x"
-        mx,my,mz = zeros(ny,nz),zeros(ny,nz),zeros(ny,nz)
-        for i = 1:nx
-            mx .+= b[2,i,:,:]
-            my .+= b[3,i,:,:]
-            mz .+= b[1,i,:,:]
-        end
-        N = nx
-    elseif axis == "y"
-        mx,my,mz = zeros(nx,nz),zeros(nx,nz),zeros(nx,nz)
-        for j = 1:ny
-            mx .-= b[1,:,j,:]
-            my .+= b[3,:,j,:]
-            mz .+= b[2,:,j,:]
-        end
-        N = ny
-    elseif axis == "z"
-        mx,my,mz = zeros(nx,ny),zeros(nx,ny),zeros(nx,ny)
-        for k = 1:nz
-            mx .+= b[1,:,:,k]
-            my .+= b[2,:,:,k]
-            mz .+= b[3,:,:,k]
-        end
-        N = nz
-    end
+    mx,my,mz = b[1,:,:,:],b[2,:,:,:],b[3,:,:,:]
 
-    return mx/N,my/N,mz/N
+    mxp,myp,mzp = Make_Projection(mx,my,mz,Nx=Nx,Ny=Ny,beta=beta,gamma=gamma,ItpNum=ItpNum)
+    return mxp,myp,mzp
 end
 
-function m_average(ovf::OVF2;axis::String="z")
+function trilinear_interpolation(M,x,y,z)
+    (nx,ny,nz)=size(M)
+    xf,yf,zf = Int(floor(x)),Int(floor(y)),Int(floor(z))
+    if x==nx || y==ny || z==nz
+        return M[xf,yf,zf]
+    end 
+    xd,yd,zd = x-xf,y-yf,z-zf
+    xp,yp,zp = 1-xd,1-yd,1-zd
+
+    v = M[xf,yf,zf]*xp*yp*zp + M[xf+1,yf+1,zf+1]*xd*yd*zd +
+        M[xf,yf,zf+1]*xp*yp*zd + M[xf,yf+1,zf]*xp*yd*zp + M[xf+1,yf,zf]*xd*yp*zp+
+        M[xf+1,yf+1,zf]*xp*yd*zp + M[xf+1,yf,zf+1]*xd*yp*zd + M[xf,yf+1,zf+1]*xp*yd*zd
+
+    return v
+end
+
+function Make_Projection(mx::Array{Float64,3},my::Array{Float64,3},mz::Array{Float64,3};
+                          Nx::Int=128,Ny::Int=128,beta::Number=0,gamma::Number=0,ItpNum::Int=0)
+
+    (nx,ny,nz) = size(mx)
+    mxp,myp,mzp = zeros(Nx,Ny),zeros(Nx,Ny),zeros(Nx,Ny)
+    eps0 = 1/(ItpNum+1)
+    eps3 = eps0*eps0*eps0
+    for i=1:nx, j= 1:ny, k=1:nz
+        for eps_i = 0:ItpNum, eps_j = 0:ItpNum, eps_k = 0:ItpNum
+            x0,y0,z0 = i+eps_i*eps0, j+eps_j*eps0, k+eps_k*eps0
+            if x0>nx+1e-6 || y0>ny+1e-6 || z0>nz+1e-6
+                continue
+            end
+
+            if eps_i==0 && eps_j==0 && eps_k==0
+                itp_mx,itp_my,itp_mz = mx[i,j,k],my[i,j,k],mz[i,j,k]
+            else
+                itp_mx = trilinear_interpolation(mx,x0,y0,z0)
+                itp_my = trilinear_interpolation(my,x0,y0,z0)
+                itp_mz = trilinear_interpolation(mz,x0,y0,z0)
+            end
+
+            x1,y1,z1= x0-(nx+1)/2,y0-(ny+1)/2,z0-(nz+1)/2
+            axis1 = [0,-1.0,0]
+            x2,y2,z2 = rotation_operator([x1,y1,z1],axis1,float(beta))
+
+            axis2 = rotation_operator([1.0,0,0],axis1,float(beta))
+            x3,y3,z3 = rotation_operator([x2,y2,z2],axis2,float(gamma))
+            x,y = x3+(Nx+1)/2,y3+(Ny+1)/2
+            linear_separate(mxp,myp,mzp,itp_mx,itp_my,itp_mz,x,y,Nx,Ny,beta,gamma)
+        end
+    end
+    return mxp*eps3,myp*eps3,mzp*eps3
+end
+
+function linear_separate(mxp,myp,mzp,mx,my,mz,x,y,Nx,Ny,beta,gamma)
+    cb,sb,cg,sg = cos(beta),sin(beta),cos(gamma),sin(gamma)
+    if 1 <= x <= Nx && 1 <= y <= Ny
+        xf,yf = Int(floor(x)),Int(floor(y))
+        xd,yd = x-xf,y-yf
+        xp,yp = 1-xd,1-yd
+        mx1,my1,mz1 = mx*cb-mz*sb, my, mz*cb+mx*sb
+        mxx,myy,mzz = mx1, my1*cg-mz1*sg, my1*sg+mz1*cg
+
+        mxp[xf,yf] += mxx*xp*yp
+        myp[xf,yf] += myy*xp*yp
+        mzp[xf,yf] += mzz*xp*yp
+
+        if yf+1 <= Ny
+            mxp[xf,yf+1] += mxx*xp*yd
+            myp[xf,yf+1] += myy*xp*yd
+            mzp[xf,yf+1] += mzz*xp*yd
+        end
+
+        if xf+1 <= Nx
+            mxp[xf+1,yf] += mxx*xd*yp
+            myp[xf+1,yf] += myy*xd*yp
+            mzp[xf+1,yf] += mzz*xd*yp
+        end
+        
+        if yf+1 <= Ny && xf+1 <= Nx
+            mxp[xf+1,yf+1] += mxx*xd*yd
+            myp[xf+1,yf+1] += myy*xd*yd
+            mzp[xf+1,yf+1] += mzz*xd*yd
+        end
+    end
+end
+
+function Make_Projection(ovf::OVF2;beta=0,gamma=0,Nx=256,Ny=256,ItpNum=0)
     m = ovf.data
     nx = ovf.xnodes
     ny = ovf.ynodes
     nz = ovf.znodes
 
-    return m_average(m,nx,ny,nz,axis=axis)
+    return Make_Projection(m,nx,ny,nz,beta=beta,gamma=gamma,Nx=Nx,Ny=Ny,ItpNum=ItpNum)
 end
+
+
