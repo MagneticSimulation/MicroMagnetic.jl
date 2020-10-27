8cc427615172e2c88516f3c79437bde14288026a
magnet <1424595185@qq.com>
2020-10-27 21:18:36 +0800

    add colorwheel

---
+++ examples/colorwheel.jl
@@ -0,0 +1,28 @@
+using JuMag
+using PyCall
+
+function save_colorwheel()
+    mesh = FDMeshGPU(nx=100,ny=100,nz=1)
+    sim = Sim(mesh)
+    set_Ms_cylindrical(sim,1e5)
+
+    function m0_fun(i,j,k,dx,dy,dz)
+        R = 50*dx
+        xc,yc = 50*dx,50*dy
+        x,y = i*dx,j*dy
+        rx,ry = x-xc,y-yc
+        r = sqrt(rx^2+ry^2)
+        if r>=R
+            return (0,0,0)
+        end
+        mx,my = rx/R,ry/R
+        mz = sqrt(1-mx^2-my^2)
+        return (mx,my,mz)
+    end
+
+    init_m0(sim,m0_fun)
+    save_ovf(sim,"colorwheel")
+    plotOVF("colorwheel.ovf",style="rgb")
+end
+
+save_colorwheel()
