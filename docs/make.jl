1431dfe9a27230b0618310615d31c0eba5dd9da4
Weiwei Wang <wangweiwei@ahu.edu.cn>
2022-09-21 20:51:18 +0800

    add docs.yml

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,27 @@
 using Documenter
+#using DemoCards
 using JuMag
 #using DocumenterTools: Themes
 
+format = Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true")
+
+PAGES = ["index.md",
+    "tutorial.md",
+    "equations.md",
+    "notes.md",
+    "functions.md",
+    "questions.md"]
+
 makedocs(
     sitename = "JuMag.jl",
-    format = Documenter.HTML(
-        #prettyurls = get(ENV, "CI", nothing) == "true"
-        prettyurls = true
-    ),
+    format = format,
     modules = [JuMag],
-    pages = Any[
-        "index.md",
-        "tutorial.md",
-        "equations.md",
-        "notes.md",
-        "functions.md",
-        "questions.md"
-        #"Examples" => ["examples/std4.md", "examples/SW.md"]
-        ],
+    pages = PAGES,
     highlightsig = true
 )
 
+
 deploydocs(
     #deps = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
-    repo = "github.com/ww1g11/JuMag.jl"
+    repo = "github.com/ww1g11/JuMag.jl.git"
 )
