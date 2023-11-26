5b43ae68d507da81fee62d7fdd8186a0353feced
Weiwei Wang <wangweiwei@ahu.edu.cn>
2023-11-26 14:39:46 +0800

    recover the previous ltem code

--- src/ltem/ltem.jl
+++ src/ltem/ltem.jl
@@ -67,6 +67,105 @@ function compute_magnetic_phase_fft(mx, my, dx, dy, Lz, Ms; Nx=-1, Ny=-1)
 end
 
 
+#Ref: "MALTS: A tool to simulate Lorentz Transmission Electron Microscopy from micromagnetic simulations" by Stephanie K. Walton
+#df in um
+#V is the Accelerating voltage, in Kv
+#V0 is the mean inner potential (MIP)
+#alpha: beam divergence angle
+#axis="x" is the normal direction
+#beta is the angle between electron beam and the normal axis, in radian
+function LTEM(ovf_name; V=300, Ms=1e5, V0=-26, df=1600, alpha=1e-5, zero_padding_size=512,axis="z",beta=0)
+    ovf = read_ovf(ovf_name)
+    nx = ovf.xnodes
+    ny = ovf.ynodes
+    nz = ovf.znodes
+    dx = ovf.xstepsize
+    dy = ovf.ystepsize
+    dz = ovf.zstepsize
+    nxyz = nx*ny*nz
+    spin = ovf.data
+    m = reshape(spin,(3, nx, ny, nz))
+
+    mx1,my1,mz1 = m_average(ovf,axis=axis)
+    mx = mx1
+    my = cos(beta)*my1-sin(beta)*mz1
+    if axis == "x"
+        nx,dx=ny,dy
+        ny,dy=nz,dz
+    elseif axis == "y"
+        ny,dy=nz,dz
+    end
+
+    dx=dx*cos(beta)
+
+    lambda, phi_E = compute_electric_phase(1000*V, V0, dz, nz, beta)
+
+    N = zero_padding_size
+    Nx = N > nx ? N : nx
+    Ny = N > ny ? N : ny
+
+    #put data on the center of zero padding square
+    new_mx = zeros(Nx, Ny)
+    new_my = zeros(Nx, Ny)
+    for i=1:nx, j=1:ny
+        new_mx[Int(floor(Nx/2-nx/2+i)),Int(floor(Ny/2-ny/2+j))] = mx[i,j]
+        new_my[Int(floor(Nx/2-nx/2+i)),Int(floor(Ny/2-ny/2+j))] = my[i,j]
+    end
+
+    fft_mx = fft(new_mx)
+    fft_my = fft(new_my)
+
+    kx = fftfreq(Nx, d=dx)
+    ky = fftfreq(Ny, d=dy)
+
+    fft_mx_ky = zeros(Complex{Float64}, (Nx,Ny))
+    fft_my_kx = zeros(Complex{Float64}, (Nx,Ny))
+    for i=1:Nx, j=1:Ny
+        fft_mx_ky[i,j] = fft_mx[i,j]*ky[j]
+        fft_my_kx[i,j] = fft_my[i,j]*kx[i]
+    end
+
+    Phi_M = zeros(Complex{Float64}, (Nx,Ny))
+    T = zeros(Complex{Float64}, (Nx,Ny))
+    E = zeros(Nx,Ny)
+    df = df*1e-6
+
+    for i=1:Nx, j=1:Ny
+        k2 = kx[i]^2 + ky[j]^2
+        k = sqrt(k2)
+        T[i,j] = exp(-pi*1im*(df*lambda*k2))
+        E[i,j] = exp(-(pi*alpha*df*k)^2)
+        if k2 > 0
+            Phi_M[i,j] = 1im*(c_e/h_bar)*mu_0*Ms*(nz*dz)*(fft_mx_ky[i,j]-fft_my_kx[i,j])/k2
+        end
+    end
+    Phi_M[1,1]=0
+
+    phi_M = 1/cos(beta)*real.(ifft(Phi_M))
+
+    phi = phi_M
+
+    for i=1:nx,j=1:ny
+        phi[Int(floor(Nx/2-nx/2+i)),Int(floor(Ny/2-ny/2+j))] += phi_E
+    end
+    phi = mod.(phi,2*pi)
+
+    fg = fft(exp.(1im.*phi))
+
+    intensity = (abs.(ifft(fg.*E.*T))).^2;
+
+    local_phi = zeros(nx,ny)
+    local_intensity = zeros(nx,ny)
+
+    for i =1:nx,j=1:ny
+        local_phi[i,j] = phi_M[Int(floor(Nx/2-nx/2+i)),Int(floor(Ny/2-ny/2+j))]
+        local_intensity[i,j] = intensity[Int(floor(Nx/2-nx/2+i)),Int(floor(Ny/2-ny/2+j))]
+    end
+
+    return local_phi, local_intensity
+end
+
+
 """
     compute_magnetic_phase(m::Array{T,4}, theta::Real, axis::String;
 N1::Int=-1, N2::Int=-1, Ms=1/mu_0, d::Real=1.0) where {T<:AbstractFloat}
