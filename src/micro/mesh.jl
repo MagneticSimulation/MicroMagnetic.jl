ecc2bda36ea3b7142a0868c5787e20a28d6d7511
Weiwei Wang <wangweiwei@ahu.edu.cn>
2024-04-04 12:59:01 +0800

    update FDMesh

--- src/micro/mesh.jl
+++ src/micro/mesh.jl
@@ -1,101 +1,33 @@
 abstract type Mesh end
-abstract type MeshCPU <: Mesh end
-abstract type MeshGPU <: Mesh end
 
-struct FDMesh <: MeshCPU
-  dx::Float64
-  dy::Float64
-  dz::Float64
-  nx::Int64
-  ny::Int64
-  nz::Int64
-  n_total::Int64
-  volume::Float64
-  ngbs::Array{Int64, 2}
-  xperiodic::Bool
-  yperiodic::Bool
-  zperiodic::Bool
-end
+export FDMesh
 
-"""
-    FDMesh(;dx=1e-9, dy=1e-9, dz=1e-9, nx=1, ny=1, nz=1, pbc="open")
-
-Create a FDMesh for given parameters. `pbc` could be any combination of "x", "y" and "z".
-"""
-function FDMesh(;dx=1e-9, dy=1e-9, dz=1e-9, nx=1, ny=1, nz=1, pbc="open")
-    ngbs = zeros(Int64, 6, nx*ny*nz)
-    xperiodic = 'x' in pbc ? true : false
-    yperiodic = 'y' in pbc ? true : false
-    zperiodic = 'z' in pbc ? true : false
-
-    for k = 1:nz, j = 1:ny, i=1:nx
-        id = index(i, j, k, nx, ny, nz)
-        ngbs[1,id] = indexpbc(i-1,j,k, nx,ny,nz, xperiodic, yperiodic, zperiodic)
-        ngbs[2,id] = indexpbc(i+1,j,k, nx,ny,nz, xperiodic, yperiodic, zperiodic)
-        ngbs[3,id] = indexpbc(i,j-1,k, nx,ny,nz, xperiodic, yperiodic, zperiodic)
-        ngbs[4,id] = indexpbc(i,j+1,k, nx,ny,nz, xperiodic, yperiodic, zperiodic)
-        ngbs[5,id] = indexpbc(i,j,k-1, nx,ny,nz, xperiodic, yperiodic, zperiodic)
-        ngbs[6,id] = indexpbc(i,j,k+1, nx,ny,nz, xperiodic, yperiodic, zperiodic)
-    end
-    volume = dx*dy*dz
-    n_total = nx*ny*nz
-    return FDMesh(dx, dy, dz, nx, ny, nz, n_total, volume, ngbs, xperiodic, yperiodic, zperiodic)
+struct FDMesh{T} <: Mesh
+    dx::T
+    dy::T
+    dz::T
+    nx::Int64
+    ny::Int64
+    nz::Int64
+    xperiodic::Bool
+    yperiodic::Bool
+    zperiodic::Bool
+    n_total::Int64
+    volume::T
+    ngbs
 end
 
 
 @inline function index(i::Int64, j::Int64, k::Int64, nx::Int64, ny::Int64, nz::Int64)
-  if i < 1 || j < 1 || k < 1 || k > nz || j > ny || i > nx
-    return -1
-  end
-  return (k-1) * nx*ny + (j-1) * nx + i
-end
-
-@inline function _x_plus_one(i::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, xperiodic::Bool)::Int64
-    if i < nx || xperiodic
-        return (i==nx) ? index +1 - nx : index +1
+    if i < 1 || j < 1 || k < 1 || k > nz || j > ny || i > nx
+        return -1
     end
-    return -1
+    return (k - 1) * nx * ny + (j - 1) * nx + i
 end
 
-@inline function _x_minus_one(i::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, xperiodic::Bool)::Int64
-    if i > 1 || xperiodic
-        return (i==1) ? index - 1 + nx : index -1
-    end
-    return -1
-end
+function indexpbc(i::Int64, j::Int64, k::Int64, nx::Int64, ny::Int64, nz::Int64,
+    xperiodic::Bool, yperiodic::Bool, zperiodic::Bool)
 
-@inline function _y_plus_one(j::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, yperiodic::Bool)::Int64
-    if j < ny || yperiodic
-        return (j==ny) ? index + nx - nx*ny : index + nx
-    end
-    return -1
-end
-
-@inline function _y_minus_one(j::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, yperiodic::Bool)::Int64
-    if j > 1 || yperiodic
-        return (j==1) ? index - nx + nx*ny : index - nx
-    end
-    return -1
-end
-
-@inline function _z_plus_one(k::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, zperiodic::Bool)::Int64
-    if k<nz || zperiodic
-        return (k==nz) ? index + nx*ny*(1-nz) : index + nx*ny
-	end
-    return -1
-end
-
-@inline function _z_minus_one(k::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, zperiodic::Bool)::Int64
-    if k>1 || zperiodic
-        return (k==1) ? index + nx*ny*(nz-1) : index - nx*ny
-	end
-    return -1
-end
-
-
-function indexpbc(i::Int64, j::Int64, k::Int64, nx::Int64, ny::Int64, nz::Int64, 
-                  xperiodic::Bool, yperiodic::Bool, zperiodic::Bool)
-                  
     xperiodic && (i < 1) && (i += nx)
     xperiodic && (i > nx) && (i -= nx)
 
@@ -106,4 +38,81 @@ function indexpbc(i::Int64, j::Int64, k::Int64, nx::Int64, ny::Int64, nz::Int64,
     zperiodic && (k > nz) && (k -= nz)
 
     return index(i, j, k, nx, ny, nz)
+end
+
+
+"""
+    FDMesh(;dx=1e-9, dy=1e-9, dz=1e-9, nx=1, ny=1, nz=1, pbc="open")
+
+Create a FDMesh for given parameters. `pbc` could be any combination of "x", "y" and "z".
+"""
+function FDMesh(; dx=1e-9, dy=1e-9, dz=1e-9, nx=1, ny=1, nz=1, pbc="open")
+
+    ngbs = zeros(Int32, 6, nx * ny * nz)
+
+    xperiodic = 'x' in pbc ? true : false
+    yperiodic = 'y' in pbc ? true : false
+    zperiodic = 'z' in pbc ? true : false
+
+    for k = 1:nz, j = 1:ny, i = 1:nx
+        id = index(i, j, k, nx, ny, nz)
+        ngbs[1, id] = indexpbc(i - 1, j, k, nx, ny, nz, xperiodic, yperiodic, zperiodic)
+        ngbs[2, id] = indexpbc(i + 1, j, k, nx, ny, nz, xperiodic, yperiodic, zperiodic)
+        ngbs[3, id] = indexpbc(i, j - 1, k, nx, ny, nz, xperiodic, yperiodic, zperiodic)
+        ngbs[4, id] = indexpbc(i, j + 1, k, nx, ny, nz, xperiodic, yperiodic, zperiodic)
+        ngbs[5, id] = indexpbc(i, j, k - 1, nx, ny, nz, xperiodic, yperiodic, zperiodic)
+        ngbs[6, id] = indexpbc(i, j, k + 1, nx, ny, nz, xperiodic, yperiodic, zperiodic)
+    end
+    volume = dx * dy * dz
+    n_total = nx * ny * nz
+    if backend[] == CPU()
+        return FDMesh(dx, dy, dz, nx, ny, nz, xperiodic, yperiodic, zperiodic, n_total, volume, ngbs)
+    end
+
+    ngbs_kb = KernelAbstractions.ones(backend[], Int32, 6, nx * ny * nz)
+    copyto!(ngbs_kb, ngbs)
+    return FDMesh(dx, dy, dz, nx, ny, nz, xperiodic, yperiodic, zperiodic, n_total, volume, ngbs_kb)
+end
+
+
+@inline function _x_plus_one(i::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, xperiodic::Bool)::Int64
+    if i < nx || xperiodic
+        return (i == nx) ? index + 1 - nx : index + 1
+    end
+    return -1
+end
+
+@inline function _x_minus_one(i::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, xperiodic::Bool)::Int64
+    if i > 1 || xperiodic
+        return (i == 1) ? index - 1 + nx : index - 1
+    end
+    return -1
+end
+
+@inline function _y_plus_one(j::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, yperiodic::Bool)::Int64
+    if j < ny || yperiodic
+        return (j == ny) ? index + nx - nx * ny : index + nx
+    end
+    return -1
+end
+
+@inline function _y_minus_one(j::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, yperiodic::Bool)::Int64
+    if j > 1 || yperiodic
+        return (j == 1) ? index - nx + nx * ny : index - nx
+    end
+    return -1
+end
+
+@inline function _z_plus_one(k::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, zperiodic::Bool)::Int64
+    if k < nz || zperiodic
+        return (k == nz) ? index + nx * ny * (1 - nz) : index + nx * ny
+    end
+    return -1
+end
+
+@inline function _z_minus_one(k::Int64, index::Int64, nx::Int64, ny::Int64, nz::Int64, zperiodic::Bool)::Int64
+    if k > 1 || zperiodic
+        return (k == 1) ? index + nx * ny * (nz - 1) : index - nx * ny
+    end
+    return -1
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,40 +1,19 @@
 using JuMag
 
 include("test_mesh.jl")
-include("test_exch.jl")
-include("test_interlayer.jl")
-include("test_demag.jl")
-include("test_demag_pbc.jl")
-include("test_zeeman.jl")
-include("test_anis.jl")
-include("test_fields.jl")
-include("test_stt.jl")
-include("test_llg.jl")
-include("test_integrator.jl")
-include("test_relax.jl")
-include("test_util.jl")
-include("test_ovf.jl")
-include("test_phase.jl")
-include("test_neb.jl")
-include("test_geometry.jl")
 
-if JuMag._cuda_available.x
-  JuMag.cuda_using_double()
-  include("cuda/test_sim.jl")
-  include("cuda/test_exch.jl")
-  include("cuda/test_demag.jl")
-  include("cuda/test_llg.jl")
-  include("cuda/test_ovf.jl")
-  include("cuda/test_neb.jl")
-
-  include("atomistic/test_llg.jl")
-  include("atomistic/test_laser.jl")
-  include("atomistic/test_next_exch.jl")
-
-  JuMag.cuda_using_double()
-  test_zeeman(gpu=true)
-  mesh =  FDMeshGPU(nx=20, ny=5, nz=3, dx=2.5e-9, dy=2.5e-9, dz=3e-9)
-  test_fields(mesh, gpu=true)
+@testset "CPU back-end" begin
+  test_FDMesh()
 end
 
-include("test_radon.jl")
+
+
+@testset "NVIDIA CUDA back-end" begin
+  if Base.find_package("CUDA") !== nothing
+    using CUDA
+
+    test_FDMesh()
+  end
+end
+
+
