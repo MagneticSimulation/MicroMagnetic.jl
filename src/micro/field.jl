2acef1dece8ef41c340318527f218170175c1b02
Weiwei Wang <wangweiwei@ahu.edu.cn>
2024-04-04 20:02:19 +0800

    anisotropy works

--- src/micro/field.jl
+++ src/micro/field.jl
@@ -22,70 +22,40 @@ function effective_field(zee::TimeZeeman, sim::MicroSim, spin, t::Float64)
   return nothing
 end
 
-
-function effective_field(anis::Anisotropy, sim::MicroSim, spin::Array{Float64, 1}, t::Float64)
-  mu0 = 4.0*pi*1e-7
-  mesh = sim.mesh
+function effective_field(anis::Anisotropy, sim::MicroSim, spin, t::Float64)
   n_total = sim.n_total
-  field = anis.field
-  energy = anis.energy
-  Ms = sim.Ms
-  Ku = anis.Ku
-  axis = anis.axis
-  for i = 1:n_total
-    if Ms[i] == 0.0
-      energy[i] = 0.0
-      field[3*i-2] = 0.0
-      field[3*i-1] = 0.0
-      field[3*i] = 0.0
-      continue
-    end
-    k = 3*(i-1)
-    sa = spin[k+1]*axis[1]+spin[k+2]*axis[2]+spin[k+3]*axis[3]
-    Ms_inv = 1.0/(Ms[i]*mu0)
-    field[k+1] = 2*Ku[i]*sa*axis[1]*Ms_inv
-    field[k+2] = 2*Ku[i]*sa*axis[2]*Ms_inv
-    field[k+3] = 2*Ku[i]*sa*axis[3]*Ms_inv
-    energy[i] = Ku[i]*(1.0-sa*sa)*mesh.volume
-  end
+  volume = sim.mesh.volume
 
+  T = single_precision.x ? Float32 : Float64
+
+  groupsize = 512
+  kernel! = anisotropy_kernel!(backend[], groupsize)
+  kernel!(spin, anis.field, anis.energy, anis.Ku, T(anis.axis[1]), T(anis.axis[2]), T(anis.axis[3]), sim.Ms, volume, ndrange=n_total)
+
+  KernelAbstractions.synchronize(backend[])
+  return nothing
 end
 
-function effective_field(anis::CubicAnisotropy, sim::MicroSim, spin::Array{Float64, 1}, t::Float64)
-  mu0 = 4.0*pi*1e-7
-  mesh = sim.mesh
+function effective_field(anis::CubicAnisotropy, sim::MicroSim, spin, t::Float64)
   n_total = sim.n_total
-  field = anis.field
-  energy = anis.energy
-  Ms = sim.Ms
-  Kc = anis.Kc
-  axis = anis.axis
-  for i = 1:n_total
-    if Ms[i] == 0.0
-      energy[i] = 0.0
-      field[3*i-2] = 0.0
-      field[3*i-1] = 0.0
-      field[3*i] = 0.0
-      continue
-    end
-    j = 3*(i-1)
-    Ms_inv = 4*Kc/(Ms[i]*mu0)
+  volume = sim.mesh.volume
 
-    mxp = axis[1]*spin[j+1] + axis[2]*spin[j+2] + axis[3]*spin[j+3]
-    myp = axis[4]*spin[j+1] + axis[5]*spin[j+2] + axis[6]*spin[j+3]
-    mzp = axis[7]*spin[j+1] + axis[8]*spin[j+2] + axis[9]*spin[j+3]
-    mxp3 = mxp*mxp*mxp
-    myp3 = myp*myp*myp
-    mzp3 = mzp*mzp*mzp
-    field[j+1] = Ms_inv*(mxp3*axis[1]+myp3*axis[4]+mzp3*axis[7])
-    field[j+2] = Ms_inv*(mxp3*axis[2]+myp3*axis[5]+mzp3*axis[8])
-    field[j+3] = Ms_inv*(mxp3*axis[3]+myp3*axis[6]+mzp3*axis[9])
-    energy[i] = -Kc*(mxp*mxp3+myp*myp3+mzp*mzp3)*mesh.volume
-  end
+  T = single_precision.x ? Float32 : Float64
+
+  groupsize = 512
+  kernel! = cubic_anisotropy_kernel!(backend[], groupsize)
+  a1x, a1y, a1z = anis.axis1
+  a2x, a2y, a2z = anis.axis2
+  a3x, a3y, a3z = anis.axis3
+  kernel!(spin, anis.field, anis.energy, anis.Kc, T(a1x), T(a1y), T(a1z), T(a2x), T(a2y), T(a2z),
+    T(a3x), T(a3y), T(a3z), sim.Ms, volume, ndrange=n_total)
+
+  KernelAbstractions.synchronize(backend[])
+  return nothing
 end
 
-function effective_field(exch::Exchange, sim::MicroSim, spin::Array{Float64, 1}, t::Float64)
-  mu0 = 4.0*pi*1e-7
+function effective_field(exch::Exchange, sim::MicroSim, spin::Array{Float64,1}, t::Float64)
+  mu0 = 4.0 * pi * 1e-7
   mesh = sim.mesh
   dx = mesh.dx
   dy = mesh.dy
@@ -96,40 +66,40 @@ function effective_field(exch::Exchange, sim::MicroSim, spin::Array{Float64, 1},
   energy = exch.energy
   A = exch.A
   Ms = sim.Ms
-  ax = 2.0  / (dx * dx)
-  ay = 2.0  / (dy * dy)
-  az = 2.0  / (dz * dz)
+  ax = 2.0 / (dx * dx)
+  ay = 2.0 / (dy * dy)
+  az = 2.0 / (dz * dz)
   nabla = (ax, ax, ay, ay, az, az)
 
   for index = 1:n_total
-    i = 3*index - 2
+    i = 3 * index - 2
     if Ms[index] == 0.0
-        energy[index] = 0.0
-        field[i] = 0.0
-        field[i+1] = 0.0
-        field[i+2] = 0.0
+      energy[index] = 0.0
+      field[i] = 0.0
+      field[i+1] = 0.0
+      field[i+2] = 0.0
       continue
     end
     fx, fy, fz = 0.0, 0.0, 0.0
-    for j=1:6
-      id = ngbs[j,index]
-      if id>0 && Ms[id]>0
-        k = 3*id-2
-        fx += A[index]*nabla[j]*(spin[k]-spin[i])
-        fy += A[index]*nabla[j]*(spin[k+1]-spin[i+1])
-        fz += A[index]*nabla[j]*(spin[k+2]-spin[i+2])
+    for j = 1:6
+      id = ngbs[j, index]
+      if id > 0 && Ms[id] > 0
+        k = 3 * id - 2
+        fx += A[index] * nabla[j] * (spin[k] - spin[i])
+        fy += A[index] * nabla[j] * (spin[k+1] - spin[i+1])
+        fz += A[index] * nabla[j] * (spin[k+2] - spin[i+2])
       end
     end
-    Ms_inv = 1.0/(Ms[index]*mu0)
-    energy[index] = -0.5*(fx*spin[i] + fy*spin[i+1] + fz*spin[i+2])*mesh.volume
-    field[i] = fx*Ms_inv
-    field[i+1] = fy*Ms_inv
-    field[i+2] = fz*Ms_inv
+    Ms_inv = 1.0 / (Ms[index] * mu0)
+    energy[index] = -0.5 * (fx * spin[i] + fy * spin[i+1] + fz * spin[i+2]) * mesh.volume
+    field[i] = fx * Ms_inv
+    field[i+1] = fy * Ms_inv
+    field[i+2] = fz * Ms_inv
   end
 end
 
-function effective_field(exch::VectorExchange, sim::MicroSim, spin::Array{Float64, 1}, t::Float64)
-  mu0 = 4.0*pi*1e-7
+function effective_field(exch::VectorExchange, sim::MicroSim, spin::Array{Float64,1}, t::Float64)
+  mu0 = 4.0 * pi * 1e-7
   mesh = sim.mesh
   dx = mesh.dx
   dy = mesh.dy
@@ -140,99 +110,99 @@ function effective_field(exch::VectorExchange, sim::MicroSim, spin::Array{Float6
   energy = exch.energy
   A = exch.A
   Ms = sim.Ms
-  ax = 2.0  / (dx * dx)
-  ay = 2.0  / (dy * dy)
-  az = 2.0  / (dz * dz)
+  ax = 2.0 / (dx * dx)
+  ay = 2.0 / (dy * dy)
+  az = 2.0 / (dz * dz)
   nabla = (ax, ax, ay, ay, az, az)
 
   for index = 1:n_total
-    i = 3*index - 2
+    i = 3 * index - 2
     if Ms[index] == 0.0
-        energy[index] = 0.0
-        field[i] = 0.0
-        field[i+1] = 0.0
-        field[i+2] = 0.0
-        continue
+      energy[index] = 0.0
+      field[i] = 0.0
+      field[i+1] = 0.0
+      field[i+2] = 0.0
+      continue
     end
 
     fx, fy, fz = 0.0, 0.0, 0.0
-    for j=1:2
-      id = ngbs[j,index]
-      if id>0 && Ms[id]>0
-        k = 3*id-2
-        fx += A[i]*nabla[j]*(spin[k]-spin[i])
-        fy += A[i]*nabla[j]*(spin[k+1]-spin[i+1])
-        fz += A[i]*nabla[j]*(spin[k+2]-spin[i+2])
+    for j = 1:2
+      id = ngbs[j, index]
+      if id > 0 && Ms[id] > 0
+        k = 3 * id - 2
+        fx += A[i] * nabla[j] * (spin[k] - spin[i])
+        fy += A[i] * nabla[j] * (spin[k+1] - spin[i+1])
+        fz += A[i] * nabla[j] * (spin[k+2] - spin[i+2])
       end
     end
-    for j=3:4
-      id = ngbs[j,index]
-      if id>0 && Ms[id]>0
-        k = 3*id-2
-        fx += A[i+1]*nabla[j]*(spin[k]-spin[i])
-        fy += A[i+1]*nabla[j]*(spin[k+1]-spin[i+1])
-        fz += A[i+1]*nabla[j]*(spin[k+2]-spin[i+2])
+    for j = 3:4
+      id = ngbs[j, index]
+      if id > 0 && Ms[id] > 0
+        k = 3 * id - 2
+        fx += A[i+1] * nabla[j] * (spin[k] - spin[i])
+        fy += A[i+1] * nabla[j] * (spin[k+1] - spin[i+1])
+        fz += A[i+1] * nabla[j] * (spin[k+2] - spin[i+2])
       end
     end
-    for j=5:6
-      id = ngbs[j,index]
-      if id>0 && Ms[id]>0
-        k = 3*id-2
-        fx += A[i+2]*nabla[j]*(spin[k]-spin[i])
-        fy += A[i+2]*nabla[j]*(spin[k+1]-spin[i+1])
-        fz += A[i+2]*nabla[j]*(spin[k+2]-spin[i+2])
+    for j = 5:6
+      id = ngbs[j, index]
+      if id > 0 && Ms[id] > 0
+        k = 3 * id - 2
+        fx += A[i+2] * nabla[j] * (spin[k] - spin[i])
+        fy += A[i+2] * nabla[j] * (spin[k+1] - spin[i+1])
+        fz += A[i+2] * nabla[j] * (spin[k+2] - spin[i+2])
       end
     end
-    Ms_inv = 1.0/(Ms[index]*mu0)
-    energy[index] = -0.5*(fx*spin[i] + fy*spin[i+1] + fz*spin[i+2])*mesh.volume
-    field[i] = fx*Ms_inv
-    field[i+1] = fy*Ms_inv
-    field[i+2] = fz*Ms_inv
+    Ms_inv = 1.0 / (Ms[index] * mu0)
+    energy[index] = -0.5 * (fx * spin[i] + fy * spin[i+1] + fz * spin[i+2]) * mesh.volume
+    field[i] = fx * Ms_inv
+    field[i+1] = fy * Ms_inv
+    field[i+2] = fz * Ms_inv
   end
 end
 
 
-function effective_field(exch::ExchangeRKKY, sim::MicroSim, spin::Array{Float64, 1}, t::Float64)
-  mu0 = 4.0*pi*1e-7
+function effective_field(exch::ExchangeRKKY, sim::MicroSim, spin::Array{Float64,1}, t::Float64)
+  mu0 = 4.0 * pi * 1e-7
   mesh = sim.mesh
   field = exch.field
   energy = exch.energy
   Ms = sim.Ms
-  sigma = exch.J/mesh.dz
+  sigma = exch.J / mesh.dz
   nx, ny, nz = mesh.nx, mesh.ny, mesh.nz
-  for i=1:nx, j=1:ny
-      id1 = (j-1) * nx + i
-      id2 = (nz-1) * nx*ny + (j-1) * nx + i
-      k1 = 3*id1-2
-      k2 = 3*id2-2
-      mbx = spin[k1]
-      mby = spin[k1+1]
-      mbz = spin[k1+2]
+  for i = 1:nx, j = 1:ny
+    id1 = (j - 1) * nx + i
+    id2 = (nz - 1) * nx * ny + (j - 1) * nx + i
+    k1 = 3 * id1 - 2
+    k2 = 3 * id2 - 2
+    mbx = spin[k1]
+    mby = spin[k1+1]
+    mbz = spin[k1+2]
 
-      mtx = spin[k2]
-      mty = spin[k2+1]
-      mtz = spin[k2+2]
+    mtx = spin[k2]
+    mty = spin[k2+1]
+    mtz = spin[k2+2]
 
-      if Ms[id1] > 0 && Ms[id2] > 0
-          Ms_inv = 1.0/(Ms[id1]*mu0)
-          field[k1] = sigma*Ms_inv*mtx
-          field[k1+1] = sigma*Ms_inv*mty
-          field[k1+2] = sigma*Ms_inv*mtz
-          energy[id1] = 0.5*sigma*(1-mtx*mbx-mty*mby-mtz*mbz)
+    if Ms[id1] > 0 && Ms[id2] > 0
+      Ms_inv = 1.0 / (Ms[id1] * mu0)
+      field[k1] = sigma * Ms_inv * mtx
+      field[k1+1] = sigma * Ms_inv * mty
+      field[k1+2] = sigma * Ms_inv * mtz
+      energy[id1] = 0.5 * sigma * (1 - mtx * mbx - mty * mby - mtz * mbz)
 
-          Ms_inv = 1.0/(Ms[id2]*mu0)
-          field[k2] = sigma*Ms_inv*mbx
-          field[k2+1] = sigma*Ms_inv*mby
-          field[k2+2] = sigma*Ms_inv*mbz
-          energy[id2] = energy[id1]
-      end
+      Ms_inv = 1.0 / (Ms[id2] * mu0)
+      field[k2] = sigma * Ms_inv * mbx
+      field[k2+1] = sigma * Ms_inv * mby
+      field[k2+2] = sigma * Ms_inv * mbz
+      energy[id2] = energy[id1]
+    end
 
   end
 
 end
 
-function effective_field(dmi::BulkDMI, sim::MicroSim, spin::Array{Float64, 1}, t::Float64)
-  mu0 = 4*pi*1e-7
+function effective_field(dmi::BulkDMI, sim::MicroSim, spin::Array{Float64,1}, t::Float64)
+  mu0 = 4 * pi * 1e-7
   mesh = sim.mesh
   dx = mesh.dx
   dy = mesh.dy
@@ -243,10 +213,10 @@ function effective_field(dmi::BulkDMI, sim::MicroSim, spin::Array{Float64, 1}, t
   energy = dmi.energy
   Ms = sim.Ms
   Dx, Dy, Dz = dmi.Dx, dmi.Dy, dmi.Dz
-  Ds = (Dx/dx, Dx/dx, Dy/dy, Dy/dy, Dz/dz, Dz/dz)
-  ax = (1.0,-1.0, 0.0, 0.0, 0.0, 0.0)
-  ay = (0.0, 0.0, 1.0,-1.0, 0.0, 0.0)
-  az = (0.0, 0.0, 0.0, 0.0, 1.0,-1.0)
+  Ds = (Dx / dx, Dx / dx, Dy / dy, Dy / dy, Dz / dz, Dz / dz)
+  ax = (1.0, -1.0, 0.0, 0.0, 0.0, 0.0)
+  ay = (0.0, 0.0, 1.0, -1.0, 0.0, 0.0)
+  az = (0.0, 0.0, 0.0, 0.0, 1.0, -1.0)
 
   Threads.@threads for i = 1:n_total
     if Ms[i] == 0.0
@@ -261,26 +231,26 @@ function effective_field(dmi::BulkDMI, sim::MicroSim, spin::Array{Float64, 1}, t
     fz = 0.0
 
     for j = 1:6
-      id = ngbs[j,i]
-      if id>0 && Ms[id]>0
-        k = 3*(id-1)+1
-        fx += Ds[j]*cross_x(ax[j],ay[j],az[j],spin[k],spin[k+1],spin[k+2]);
-        fy += Ds[j]*cross_y(ax[j],ay[j],az[j],spin[k],spin[k+1],spin[k+2]);
-        fz += Ds[j]*cross_z(ax[j],ay[j],az[j],spin[k],spin[k+1],spin[k+2]);
+      id = ngbs[j, i]
+      if id > 0 && Ms[id] > 0
+        k = 3 * (id - 1) + 1
+        fx += Ds[j] * cross_x(ax[j], ay[j], az[j], spin[k], spin[k+1], spin[k+2])
+        fy += Ds[j] * cross_y(ax[j], ay[j], az[j], spin[k], spin[k+1], spin[k+2])
+        fz += Ds[j] * cross_z(ax[j], ay[j], az[j], spin[k], spin[k+1], spin[k+2])
       end
     end
 
-    Ms_inv = 1.0/(Ms[i]*mu0)
-    energy[i] = -0.5*(fx*spin[3*i-2] + fy*spin[3*i-1] + fz*spin[3*i])*mesh.volume
-    field[3*i-2] = fx*Ms_inv
-    field[3*i-1] = fy*Ms_inv
-    field[3*i] = fz*Ms_inv
+    Ms_inv = 1.0 / (Ms[i] * mu0)
+    energy[i] = -0.5 * (fx * spin[3*i-2] + fy * spin[3*i-1] + fz * spin[3*i]) * mesh.volume
+    field[3*i-2] = fx * Ms_inv
+    field[3*i-1] = fy * Ms_inv
+    field[3*i] = fz * Ms_inv
   end
 end
 
 
-function effective_field(dmi::InterfacialDMI, sim::MicroSim, spin::Array{Float64, 1}, t::Float64)
-  mu0 = 4*pi*1e-7
+function effective_field(dmi::InterfacialDMI, sim::MicroSim, spin::Array{Float64,1}, t::Float64)
+  mu0 = 4 * pi * 1e-7
   mesh = sim.mesh
   dx = mesh.dx
   dy = mesh.dy
@@ -291,9 +261,9 @@ function effective_field(dmi::InterfacialDMI, sim::MicroSim, spin::Array{Float64
   energy = dmi.energy
   Ms = sim.Ms
   D = dmi.D
-  Ds = (D/dx, D/dx, D/dy, D/dy)
+  Ds = (D / dx, D / dx, D / dy, D / dy)
   ax = (0.0, 0.0, -1.0, 1.0) #Dij = D r_ij x z
-  ay = (1.0,-1.0, 0.0, 0.0)
+  ay = (1.0, -1.0, 0.0, 0.0)
   az = (0.0, 0.0, 0.0, 0.0)
 
   Threads.@threads for i = 1:n_total
@@ -309,20 +279,20 @@ function effective_field(dmi::InterfacialDMI, sim::MicroSim, spin::Array{Float64
     fz = 0.0
 
     for j = 1:4
-      id = ngbs[j,i]
-      if id>0 && Ms[id]>0
-        k = 3*(id-1)+1
-        fx += Ds[j]*cross_x(ax[j],ay[j],az[j],spin[k],spin[k+1],spin[k+2]);
-        fy += Ds[j]*cross_y(ax[j],ay[j],az[j],spin[k],spin[k+1],spin[k+2]);
-        fz += Ds[j]*cross_z(ax[j],ay[j],az[j],spin[k],spin[k+1],spin[k+2]);
+      id = ngbs[j, i]
+      if id > 0 && Ms[id] > 0
+        k = 3 * (id - 1) + 1
+        fx += Ds[j] * cross_x(ax[j], ay[j], az[j], spin[k], spin[k+1], spin[k+2])
+        fy += Ds[j] * cross_y(ax[j], ay[j], az[j], spin[k], spin[k+1], spin[k+2])
+        fz += Ds[j] * cross_z(ax[j], ay[j], az[j], spin[k], spin[k+1], spin[k+2])
       end
     end
 
-    Ms_inv = 1.0/(Ms[i]*mu0)
-    energy[i] = -0.5*(fx*spin[3*i-2] + fy*spin[3*i-1] + fz*spin[3*i])*mesh.volume
-    field[3*i-2] = fx*Ms_inv
-    field[3*i-1] = fy*Ms_inv
-    field[3*i] = fz*Ms_inv
+    Ms_inv = 1.0 / (Ms[i] * mu0)
+    energy[i] = -0.5 * (fx * spin[3*i-2] + fy * spin[3*i-1] + fz * spin[3*i]) * mesh.volume
+    field[3*i-2] = fx * Ms_inv
+    field[3*i-1] = fy * Ms_inv
+    field[3*i] = fz * Ms_inv
   end
 end
 
@@ -360,7 +330,7 @@ function effective_field(sim::AbstractSim, spin, t::Float64)
   return 0
 end
 
-function compute_system_energy(sim::AbstractSim, spin::Array{Float64, 1}, t::Float64)
+function compute_system_energy(sim::AbstractSim, spin::Array{Float64,1}, t::Float64)
   #sim.total_energy = 0
   fill!(sim.energy, 0.0)
   for interaction in sim.interactions
--- test/test_anis.jl
+++ test/test_anis.jl
@@ -1,39 +1,55 @@
 using JuMag
 using Test
+using LinearAlgebra
 
-#Test mesh
-function test_anis(;gpu=false)
-    if gpu
-        mesh =  FDMeshGPU(nx=10, ny=1, nz=1)
-    else
-        mesh =  FDMesh(nx=10, ny=1, nz=1)
-    end
+function test_anis()
+
+    mesh = FDMesh(nx=10, ny=1, nz=1)
+
+    sim = Sim(mesh)
+    Ms = 8.6e5
+    set_Ms(sim, Ms)
+
+    m = (1.1, 2.3, 4.3)
+    init_m0(sim, m, norm=false)
+
+    Ku = 1e5
+    axis = (0, 0.6, 0.8)
+    anis = add_anis(sim, Ku, axis=axis)
+
+    JuMag.effective_field(sim, sim.spin, 0.0)
+
+    field = Array(anis.field)
+    energy = Array(anis.energy)
+
+    @test isapprox(field[1], 0)
+    @test isapprox(field[2], 2 * Ku / (JuMag.mu_0 * Ms) * dot(m, axis) * axis[2])
+    @test isapprox(field[3], 2 * Ku / (JuMag.mu_0 * Ms) * dot(m, axis) * axis[3])
+    @test isapprox(energy[10], Ku * (1 - dot(m, axis)^2) * 1e-27)
+end
+
+
+function test_cubic_anis()
+
+    mesh = FDMesh(nx=10, ny=1, nz=1)
 
     sim = Sim(mesh)
     Ms = 8.6e5
     Kc = 1e3
     set_Ms(sim, Ms)
-    init_m0(sim, (0.6,0.8,0), norm=false)
+    init_m0(sim, (0.6, 0.8, 0), norm=false)
 
     anis = add_cubic_anis(sim, Kc)
-    if gpu
-        JuMag.compute_fields_to_gpu(sim, sim.spin, 0.0)
-    else
-        JuMag.effective_field(sim, sim.spin, 0.0)
-    end
+
+    JuMag.effective_field(sim, sim.spin, 0.0)
 
     field = Array(anis.field)
-    energy = 
+    energy = Array(anis.energy)
 
-    @test isapprox(field[1], 1/(JuMag.mu_0*Ms)*4*Kc*0.6^3)
-    @test isapprox(field[2], 1/(JuMag.mu_0*Ms)*4*Kc*0.8^3)
-    @test isapprox(field[3], 1/(JuMag.mu_0*Ms)*4*Kc*0^3)
-    @test isapprox(anis.energy[1], -Kc*(0.6^4+0.8^4)*1e-27)
+    @test isapprox(field[1], 1 / (JuMag.mu_0 * Ms) * 4 * Kc * 0.6^3)
+    @test isapprox(field[2], 1 / (JuMag.mu_0 * Ms) * 4 * Kc * 0.8^3)
+    @test isapprox(field[3], 1 / (JuMag.mu_0 * Ms) * 4 * Kc * 0^3)
+    @test isapprox(energy[1], -Kc * (0.6^4 + 0.8^4) * 1e-27)
 end
 
-test_anis()
-
-if JuMag._cuda_available.x
-    JuMag.cuda_using_double()
-    test_anis(gpu=true)
-end
+test_cubic_anis()
