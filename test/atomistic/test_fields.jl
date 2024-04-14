13e80a8f595c29eeb066635fc074d4dc5fd15040
Weiwei Wang <wangweiwei@ahu.edu.cn>
2024-04-14 18:22:12 +0800

    fix the tests

--- test/atomistic/test_fields.jl
+++ test/atomistic/test_fields.jl
@@ -16,7 +16,7 @@ function load_field_data()
     return Ms, m0, demag, exch, dmi, anis, data[N, 3:6]
 end
 
-function test_fields(A=1.3e-11, D=4e-3, DI=2e-3, Ku=-3e4, axis=(0, 0, 1))
+function test_atomistic_fields(A=1.3e-11, D=4e-3, DI=2e-3, Ku=-3e4, axis=(0, 0, 1))
     mesh = CubicMesh(; nx=39, ny=11, nz=3, dx=0.5e-9, dy=0.5e-9, dz=0.5e-9)
 
     mu_s, m0, fd, fe, fdmi, fan, energy = load_field_data()
@@ -41,19 +41,19 @@ function test_fields(A=1.3e-11, D=4e-3, DI=2e-3, Ku=-3e4, axis=(0, 0, 1))
 
     delta_anis = 5e-3 * J * sum(mu_s .> 0)
     @test (abs((sum(demag.energy) - energy[1]) / energy[1])) < 1e-11
-    @test (abs((sum(exch.energy) - energy[2]) / energy[2])) < 1e-15
-    @test (abs((sum(dmi.energy) - energy[3]) / energy[3])) < 1e-15
-    @test (abs((sum(anis.energy) - delta_anis - energy[4]) / energy[4])) < 1e-15
+    @test (abs((sum(exch.energy) - energy[2]) / energy[2])) < 1e-14
+    @test (abs((sum(dmi.energy) - energy[3]) / energy[3])) < 1e-14
+    @test (abs((sum(anis.energy) - delta_anis - energy[4]) / energy[4])) < 1e-14
 end
 
 @testset "Test Atomistic Fields CPU" begin
     set_backend("cpu")
-    test_fields()
+    test_atomistic_fields()
 end
 
 @testset "Test Atomistic Fields CUDA" begin
     if Base.find_package("CUDA") !== nothing
         using CUDA
-        test_fields()
+        test_atomistic_fields()
     end
 end
