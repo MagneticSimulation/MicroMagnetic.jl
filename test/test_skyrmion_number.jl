0f093eafa9d8a62689988a8e544695723d00f82d
Weiwei Wang <wangweiwei@ahu.edu.cn>
2024-05-26 16:33:07 +0800

    fix the test and update Project.toml

--- test/test_skyrmion_number.jl
+++ test/test_skyrmion_number.jl
@@ -13,7 +13,7 @@ function m0_fun(i,j,k,dx,dy,dz)
   return (0,0,1)
 end
 
-function relax_system()
+function test_skyrmion_number()
   mesh =  FDMesh(nx=100, ny=100, nz=1, dx=2e-9, dy=2e-9, dz=2e-9, pbc="xy")
   sim = Sim(mesh, driver="SD", name="sim")
   set_Ms(sim, 8e5)
@@ -31,12 +31,11 @@ function relax_system()
   Rxs, Rys = compute_guiding_center(m, mesh)
   println(sum(v)," ", Rxs, " ", Rxs)
   Q = sum(v)
-  @test abs(Q+1) < 1e-15
+  @test abs(Q+1) < 1e-6
   @test abs(Rxs[1]-100e-9)<1e-9
   @test abs(Rys[1]-100e-9)<1e-9
 
 end
 
 @using_gpu()
-test_functions("skx_number", relax_system)
-
+test_functions("skx_number", test_skyrmion_number)
