156ca0226ad5aa35c684d99513141db23e2f8b9e
Weiwei Wang <wangweiwei@ahu.edu.cn>
2024-08-31 11:27:02 +0800

    update docs

--- docs/tutorials/micromagnetics/nanobar.jl
+++ docs/tutorials/micromagnetics/nanobar.jl
@@ -1,5 +1,5 @@
 # ---
-# title: Magnetization state of a nanobar
+# title: Nanobar
 # author: Weiwei Wang
 # date: 2024-05-16
 # description: an example to demostrate how to obtain the magnetization distribution in MicroMagnetic.
@@ -7,6 +7,7 @@
 # ---
 
 # In this example, we consider a nanobar with dimensions 60nm x 10nm x 5 nm
+# We used this example to demostrate how to obtain the magnetization distribution using MicroMagnetic.jl
 # We first import MicroMagnetic and CairoMakie for plotting.
 using MicroMagnetic
 using CairoMakie
@@ -14,11 +15,16 @@ using CairoMakie
 # We create a FDMesh
 mesh = FDMesh(; dx=2e-9, dy=2e-9, dz=2.5e-9, nx=30, ny=5, nz=2);
 
-# We create a Sim wth `SD` driver using `create_sim` function.
-# We consider two energies (i.e., exchange and demag) with exchange constant A = 1e-12 J/m.
-# The saturation magnetization Ms=8e5 A/m
-sim = create_sim(mesh; driver="SD", name="bar", Ms=8e5, A=1e-12, demag=true, m0=(1, 1, 0));
+# We create a Sim wth `SD` driver using `Sim` function and set the saturation magnetization Ms
+sim = Sim(mesh; driver="SD")
+set_Ms(sim, 8e5)   #Set saturation magnetization Ms=8e5 A/m
 
+# We consider two energies (i.e., exchange and demag) with exchange constant A = 1e-12 J/m.
+add_exch(sim, 1e-12);  #Add exchange interaction
+add_demag(sim);         #Add demagnetization 
+
+# Initilize the magnetization to (1,1,0) direction,
+init_m0(sim, (1, 1, 0))  #Initialize magnetization
 # We can plot the magnetization using `plot_m` function
 plot_m(sim)
 
@@ -26,7 +32,7 @@ plot_m(sim)
 # typically its value should be in the rangle of [0.01, 1]. 
 relax(sim; max_steps=2000, stopping_dmdt=0.01)
 
-# We plot the magnetization
+# We plot the magnetization again
 fig = plot_m(sim)
 
 # We save the figure to png. 
@@ -34,3 +40,22 @@ save("bar.png", fig)
 
 # Save the magnetization state for later postprocessing, which can be visualization using Paraview (https://www.paraview.org/)
 save_vtk(sim, "bar"; fields=["exch", "demag"])
+
+# ## Using the sim_with function.
+# We can use sim_with to simplify the setup of the simulation.
+# We put all the parameters together:
+args = (
+    task = "Relax",
+    mesh = FDMesh(dx=2e-9, dy=2e-9, dz=2.5e-9, nx=30, ny=5, nz=2),
+    Ms = 8e5, 
+    A = 1e-12,
+    demag = true,
+    m0 = (1, 1, 0),
+    stopping_dmdt = 0.01
+);
+
+# then use the sim_with function
+sim = sim_with(args);
+
+# We plot the magnetization 
+plot_m(sim)
--- docs/tutorials/micromagnetics/vortex.jl
+++ docs/tutorials/micromagnetics/vortex.jl
@@ -10,6 +10,8 @@
 using MicroMagnetic
 using CairoMakie
 
+
+
 # In this example, the studied system is a round nanodisk. Since we are using finite different method,
 # we create a FDMesh with dimensions 200nm x 200nm x 20 nm. 
 mesh = FDMesh(; dx=2e-9, dy=2e-9, dz=5e-9, nx=100, ny=100, nz=4);
@@ -29,10 +31,23 @@ function init_fun(i, j, k, dx, dy, dz)
     return (y / r, -x / r, 0)
 end
 
-# Create a Sim instance and then relax the system
-sim = create_sim(mesh; shape=geo, Ms=8e5, A=1.3e-11, demag=true, m0=init_fun);
-relax(sim; max_steps=5000, stopping_dmdt=0.1)
-save_vtk(sim, "vortex")
+
+# We define the 
+args = (
+    task = "Relax",
+    mesh = mesh,
+    shape = geo,
+    Ms = 8e5, 
+    A = 1.3e-11,
+    demag = true,
+    m0 = init_fun,
+    stopping_dmdt = 0.01
+);
+
+sim = sim_with(args);
 
 # We plot the magnetization distribution using the plot_m function
 plot_m(sim; figsize=(400, 400), arrows=(30, 30))
+
+# we save the vtk as well
+save_vtk(sim, "vortex")
