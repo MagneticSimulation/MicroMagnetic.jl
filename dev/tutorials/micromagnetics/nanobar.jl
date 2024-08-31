b9fbd4ef533a475891c539a2c86ef51a18d4df77
Documenter.jl <documenter@juliadocs.github.io>
2024-08-31 06:46:01 +0000

    build based on 302ba92

--- dev/tutorials/micromagnetics/nanobar.jl
+++ dev/tutorials/micromagnetics/nanobar.jl
@@ -3,7 +3,13 @@ using CairoMakie
 
 mesh = FDMesh(; dx=2e-9, dy=2e-9, dz=2.5e-9, nx=30, ny=5, nz=2);
 
-sim = create_sim(mesh; driver="SD", name="bar", Ms=8e5, A=1e-12, demag=true, m0=(1, 1, 0));
+sim = Sim(mesh; driver="SD")
+set_Ms(sim, 8e5)   #Set saturation magnetization Ms=8e5 A/m
+
+add_exch(sim, 1e-12);  #Add exchange interaction
+add_demag(sim);         #Add demagnetization
+
+init_m0(sim, (1, 1, 0))  #Initialize magnetization
 
 plot_m(sim)
 
@@ -14,5 +20,19 @@ fig = plot_m(sim)
 save("bar.png", fig)
 
 save_vtk(sim, "bar"; fields=["exch", "demag"])
+
+args = (
+    task = "Relax",
+    mesh = FDMesh(dx=2e-9, dy=2e-9, dz=2.5e-9, nx=30, ny=5, nz=2),
+    Ms = 8e5,
+    A = 1e-12,
+    demag = true,
+    m0 = (1, 1, 0),
+    stopping_dmdt = 0.01
+);
+
+sim = sim_with(args);
+
+plot_m(sim)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
--- dev/tutorials/micromagnetics/vortex.jl
+++ dev/tutorials/micromagnetics/vortex.jl
@@ -5,20 +5,29 @@ mesh = FDMesh(; dx=2e-9, dy=2e-9, dz=5e-9, nx=100, ny=100, nz=4);
 
 geo = Cylinder(; radius=100e-9);
 
-function init_fun(i, j, k, dx, dy, dz)
-    x = i - 50.5
-    y = j - 50.5
-    r = (x^2 + y^2)^0.5
-    if r < 20
+function init_fun(x, y, z)
+    r = sqrt(x^2 + y^2)
+    if r < 20e-9
         return (0, 0, 1)
     end
     return (y / r, -x / r, 0)
 end
 
-sim = create_sim(mesh; shape=geo, Ms=8e5, A=1.3e-11, demag=true, m0=init_fun);
-relax(sim; max_steps=5000, stopping_dmdt=0.1)
-save_vtk(sim, "vortex")
+args = (
+    task = "Relax",
+    mesh = mesh,
+    shape = geo,
+    Ms = 8e5,
+    A = 1.3e-11,
+    demag = true,
+    m0 = init_fun,
+    stopping_dmdt = 0.01
+);
+
+sim = sim_with(args);
 
 plot_m(sim; figsize=(400, 400), arrows=(30, 30))
+
+save_vtk(sim, "vortex")
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
