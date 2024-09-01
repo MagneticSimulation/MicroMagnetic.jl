31bb429f918b6fae0124565e3ab9d6fbb69127d3
Weiwei Wang <wangweiwei@ahu.edu.cn>
2024-09-01 11:48:32 +0800

    update fig7

--- figure_scripts/fig7/M_T_curve.jl
+++ figure_scripts/fig7/M_T_curve.jl
@@ -1,55 +1,51 @@
-#======================
-In MicroMagnetic, we can use Monte Carlo to compute the M-T curve. 
-For the atomistic model with $z$ nearest neighbors, the relation between exchange constant and $T_c$ reads [1]
-
-```math
-J = \frac{3 k_B T_c}{ \epsilon z }
-```
-
-where $\epsilon$ is a correction factor. For 3D classical Heisenberg model $\epsilon \approx 0.719$. 
-In this example, we will assume $J=300k_B$ which gives $T_c = 431 K$. The full script is shown below.
-======================#
 using MicroMagnetic
 
 @using_gpu()
 
-function relax_system_single(T)
-    mesh = CubicMesh(; nx=30, ny=30, nz=30, pbc="xyz")
+function relax_system_single(T; N=30, M=2000, m0=nothing)
+    mesh = CubicMesh(; nx=N, ny=N, nz=N, pbc="xyz")
     sim = MonteCarlo(mesh; name="mc")
-    init_m0(sim, (0, 0, 1))
-
+    
     add_exch(sim; J=300 * k_B)
-    add_dmi(sim; D=0)
-    add_zeeman(sim; Hx=0, Hy=0, Hz=0)
-    add_anis(sim; Ku=0, Kc=0)
 
-    sim.T = 100000
-    run_sim(sim; max_steps=10000, save_vtk_every=-1, save_m_every=-1)
+    if m0 == nothing
+        init_m0_random(sim)
+        sim.T = 100000
+        run_sim(sim; max_steps=10000, save_vtk_every=-1, save_m_every=-1)
+    else
+        init_m0(sim, m0)
+    end
+
     sim.T = T
     run_sim(sim; max_steps=50000, save_vtk_every=-1, save_m_every=-1)
 
-    ms = zeros(1000)
+    ms = zeros(M)
+    
     sim.T = T
-    for i in 1:1000
+    for i in 1:M
         run_sim(sim; max_steps=100, save_vtk_every=-1, save_m_every=-1)
-        t = MicroMagnetic.average_m(sim)
-        ms[i] = sqrt(t[1]^2 + t[2]^2 + t[3]^2)
+        m = MicroMagnetic.average_m(sim)
+        ms[i] = sqrt(m[1]^2 + m[2]^2 + m[3]^2)
     end
-    return sum(ms) / length(ms)
+    
+    m_average = sum(ms) / M
+    m2_average = sum(ms.^2) / M
+
+    return m_average, N^3*(m2_average-m_average^2)/T, Array(sim.spin)
 end
 
 function relax_system()
-    f = open("assets/M_H.txt", "w")
-    write(f, "#T(K)     m \n")
-    for T in 10:20:20
+    f = open("assets/M_T.txt", "w")
+    write(f, "#T(K)     m    chi\n")
+    
+    Ts = [600:-20:460; 455:-5:405; 400:-20:10]
+    m0 = nothing
+    for T in Ts
         println("Running for $T ...")
-        m = relax_system_single(T)
-        write(f, "$T    $m \n")
+        m, chi, m0 = relax_system_single(T, m0=m0)
+        write(f, "$T    $m   $chi\n")
     end
     return close(f)
 end
 
-# Run the relax_system function.
-if filesize("assets/M_H.txt") == 0
-    relax_system()
-end
+relax_system()
--- figure_scripts/fig7/plot.jl
+++ figure_scripts/fig7/plot.jl
@@ -1,24 +1,49 @@
-using DelimitedFiles
 using CairoMakie
+using DelimitedFiles
 
 function plot_m_H()
-    fig = Figure(; size=(400, 300))
-    ax = Axis(fig[1, 1]; xlabel="T (K)", ylabel="m")
 
-    data = readdlm("assets/M_H.txt"; skipstart=1)
-    sc1 = scatter!(ax, data[:, 1], data[:, 2]; markersize=10, label="M-T curve")
-    sc1.color = :transparent
-    sc1.strokewidth = 1
-    sc1.strokecolor = :purple
-    lines!(ax, data[:, 1], data[:, 2])
+  fig = Figure(size = (500, 300))
+  
+  ax1 = Axis(fig[1, 1],
+      xlabel = "T (K)",
+      ylabel = "m"
+  )
 
-    vlines!(ax, 431; linewidth=1, color=:black, linestyle=:dash)
+  ax2 = Axis(fig[1, 1],
+      xlabel = "T (K)",
+      ylabel = L"$\chi$",
+      yaxisposition = :right
+  )
 
-    axislegend()
+  ax2.yticklabelcolor = :blue
+  ax2.ylabelcolor = :blue
 
-    save("M_T.pdf", fig)
+  data = readdlm("assets/M_T.txt", skipstart=1)
+  
+  sc1 = scatter!(ax1, data[:, 1], data[:, 2], marker=:rect, markersize = 10)
+  sc1.color = :transparent
+  sc1.strokewidth = 1
+  sc1.strokecolor = :purple
+  lines!(ax1, data[:, 1], data[:, 2], color=:purple)
+  
 
-    return fig
+  sc2 = scatter!(ax2, data[:, 1], data[:, 3], markersize = 8)
+  sc2.color = :transparent
+  sc2.strokewidth = 1
+  sc2.strokecolor = :blue
+  lines!(ax2, data[:, 1], data[:, 3], color=:blue)
+
+
+  vlines!(ax1, 431, linewidth=1, color=:black, linestyle=:dash)
+  
+  axislegend(ax1, [sc1, sc2], ["m", L"$\chi$"], position=:lc)
+  
+  save("M_T.pdf", fig)
+  save("M_T.png", fig)
+
+  return fig
+
 end
 
 plot_m_H()
