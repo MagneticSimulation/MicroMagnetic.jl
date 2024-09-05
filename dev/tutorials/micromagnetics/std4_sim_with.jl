da6c9f7a3ecff9ba47906034dc00d5090b17259f
Documenter.jl <documenter@juliadocs.github.io>
2024-09-05 05:02:23 +0000

    build based on 76ed51e

--- dev/tutorials/micromagnetics/std4_sim_with.jl
+++ dev/tutorials/micromagnetics/std4_sim_with.jl
@@ -2,21 +2,17 @@ using MicroMagnetic
 
 @using_gpu()
 
-args = (
-    name = "std4",
-    task_s = ["relax", "dynamics"],           # List of tasks
-    mesh = FDMesh(nx=200, ny=50, nz=1, dx=2.5e-9, dy=2.5e-9, dz=3e-9),
-    Ms = 8e5,                                 # Saturation magnetization
-    A = 1.3e-11,                              # Exchange constant
-    demag = true,                             # Enable demagnetization
-    m0 = (1, 0.25, 0.1),                      # Initial magnetization
-    alpha = 0.02,                             # Gilbert damping
-    steps = 100,                              # Number of steps for dynamics
-    dt = 0.01ns,                              # Time step size
-    stopping_dmdt = 0.01,                     # Stopping criterion for relaxation
-    dynamic_m_interval = 1,                   # Save magnetization at each step
-    H_s = [(0,0,0), (-24.6mT, 4.3mT, 0)]      # Static field sweep
-);
+args = (name="std4", task_s=["relax", "dynamics"],           # List of tasks
+        mesh=FDMesh(; nx=200, ny=50, nz=1, dx=2.5e-9, dy=2.5e-9, dz=3e-9), Ms=8e5,                                 # Saturation magnetization
+        A=1.3e-11,                              # Exchange constant
+        demag=true,                             # Enable demagnetization
+        m0=(1, 0.25, 0.1),                      # Initial magnetization
+        alpha=0.02,                             # Gilbert damping
+        steps=100,                              # Number of steps for dynamics
+        dt=0.01ns,                              # Time step size
+        stopping_dmdt=0.01,                     # Stopping criterion for relaxation
+        dynamic_m_interval=1,                   # Save magnetization at each step
+        H_s=[(0, 0, 0), (-24.6mT, 4.3mT, 0)]);
 
 sim = sim_with(args);
 
@@ -32,18 +28,18 @@ function plot_m_ts()
     oommf = readdlm("assets/std4_oommf.txt")
 
     #Create a figure for the plot
-    fig = Figure(size=(800, 480))
-    ax = Axis(fig[1, 1], xlabel="Time (ns)", ylabel="m")
+    fig = Figure(; size=(800, 480))
+    ax = Axis(fig[1, 1]; xlabel="Time (ns)", ylabel="m")
 
     #Plot OOMMF results
-    lines!(ax, oommf[:, 1] * 1e9, oommf[:, 2], label="OOMMF")
+    lines!(ax, oommf[:, 1] * 1e9, oommf[:, 2]; label="OOMMF")
     lines!(ax, oommf[:, 1] * 1e9, oommf[:, 3])
     lines!(ax, oommf[:, 1] * 1e9, oommf[:, 4])
 
     #Plot MicroMagnetic results
-    scatter!(ax, data["time"] * 1e9, data["m_x"], markersize=6, label="MicroMagnetic.jl")
-    scatter!(ax, data["time"] * 1e9, data["m_y"], markersize=6)
-    scatter!(ax, data["time"] * 1e9, data["m_z"], markersize=6)
+    scatter!(ax, data["time"] * 1e9, data["m_x"]; markersize=6, label="MicroMagnetic.jl")
+    scatter!(ax, data["time"] * 1e9, data["m_y"]; markersize=6)
+    scatter!(ax, data["time"] * 1e9, data["m_z"]; markersize=6)
 
     #Add legend to the plot
     axislegend()
