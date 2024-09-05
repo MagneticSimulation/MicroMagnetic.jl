7a1e94f7a1489a73a178a1c9605db5f071606d20
Weiwei Wang <wangweiwei@ahu.edu.cn>
2024-09-05 12:43:54 +0800

    add save_item to run_sim function

--- docs/tutorials/micromagnetics/std4_sim_with.jl
+++ docs/tutorials/micromagnetics/std4_sim_with.jl
@@ -16,28 +16,23 @@ using MicroMagnetic
 
 # Define the system geometry: a film with thickness t = 3 nm, length L = 500 nm, and width d = 125 nm.
 # Gather all the parameters related to standard problem 4:
-args = (
-    name = "std4",
-    task_s = ["relax", "dynamics"],           # List of tasks
-    mesh = FDMesh(nx=200, ny=50, nz=1, dx=2.5e-9, dy=2.5e-9, dz=3e-9),
-    Ms = 8e5,                                 # Saturation magnetization
-    A = 1.3e-11,                              # Exchange constant
-    demag = true,                             # Enable demagnetization
-    m0 = (1, 0.25, 0.1),                      # Initial magnetization
-    alpha = 0.02,                             # Gilbert damping
-    steps = 100,                              # Number of steps for dynamics
-    dt = 0.01ns,                              # Time step size
-    stopping_dmdt = 0.01,                     # Stopping criterion for relaxation
-    dynamic_m_interval = 1,                   # Save magnetization at each step
-    H_s = [(0,0,0), (-24.6mT, 4.3mT, 0)]      # Static field sweep
-);
+args = (name="std4", task_s=["relax", "dynamics"],           # List of tasks
+        mesh=FDMesh(; nx=200, ny=50, nz=1, dx=2.5e-9, dy=2.5e-9, dz=3e-9), Ms=8e5,                                 # Saturation magnetization
+        A=1.3e-11,                              # Exchange constant
+        demag=true,                             # Enable demagnetization
+        m0=(1, 0.25, 0.1),                      # Initial magnetization
+        alpha=0.02,                             # Gilbert damping
+        steps=100,                              # Number of steps for dynamics
+        dt=0.01ns,                              # Time step size
+        stopping_dmdt=0.01,                     # Stopping criterion for relaxation
+        dynamic_m_interval=1,                   # Save magnetization at each step
+        H_s=[(0, 0, 0), (-24.6mT, 4.3mT, 0)]);
 
 # Run the simulation using the `sim_with` function:
 sim = sim_with(args);
 
 # With the above code, the simulation for standard problem 4 is complete. Next, we proceed to process the data, 
 # such as visualizing the magnetization distribution or creating a movie from the simulation results.
-
 
 # Visualize the magnetization dynamics using CairoMakie
 using CairoMakie
@@ -47,9 +42,7 @@ jld2movie("std4.jld2"; output="assets/std4.mp4", component='x');
 # Display the generated movie
 # ![](./assets/std4.mp4)
 
-
 # Finally, we plot the time evolution of the magnetization components to compare the results with OOMMF simulations.
-
 
 # Import necessary functions to plot the time evolution of magnetization
 using DelimitedFiles
@@ -60,18 +53,18 @@ function plot_m_ts()
     oommf = readdlm("assets/std4_oommf.txt")
 
     #Create a figure for the plot
-    fig = Figure(size=(800, 480))
-    ax = Axis(fig[1, 1], xlabel="Time (ns)", ylabel="m")
+    fig = Figure(; size=(800, 480))
+    ax = Axis(fig[1, 1]; xlabel="Time (ns)", ylabel="m")
 
     #Plot OOMMF results
-    lines!(ax, oommf[:, 1] * 1e9, oommf[:, 2], label="OOMMF")
+    lines!(ax, oommf[:, 1] * 1e9, oommf[:, 2]; label="OOMMF")
     lines!(ax, oommf[:, 1] * 1e9, oommf[:, 3])
     lines!(ax, oommf[:, 1] * 1e9, oommf[:, 4])
 
     #Plot MicroMagnetic results
-    scatter!(ax, data["time"] * 1e9, data["m_x"], markersize=6, label="MicroMagnetic.jl")
-    scatter!(ax, data["time"] * 1e9, data["m_y"], markersize=6)
-    scatter!(ax, data["time"] * 1e9, data["m_z"], markersize=6)
+    scatter!(ax, data["time"] * 1e9, data["m_x"]; markersize=6, label="MicroMagnetic.jl")
+    scatter!(ax, data["time"] * 1e9, data["m_y"]; markersize=6)
+    scatter!(ax, data["time"] * 1e9, data["m_z"]; markersize=6)
 
     #Add legend to the plot
     axislegend()
