6db4b6a6ea6dc4d50967f9b416f7e0bafcb556ef
Documenter.jl <documenter@juliadocs.github.io>
2024-11-19 15:13:06 +0000

    build based on 7d2b2ed

--- dev/tutorials/atomistic/skyrmion.jl
+++ dev/tutorials/atomistic/skyrmion.jl
@@ -1,39 +1,42 @@
 using MicroMagnetic
 using CairoMakie
 
-mesh = CubicMesh(; nx=120, ny=120, nz=1, pbc="xy");
+mesh = CubicMesh(; nx=50, ny=50, nz=1, pbc="xy")
 
 function m0_fun(i, j, k, dx, dy, dz)
-    r2 = (i - 60)^2 + (j - 60)^2
+    r2 = (i - 25)^2 + (j - 25)^2
     if r2 < 10^2
-        return (0.1, 0, -1)
+        return (0.01, 0, -1)
     end
     return (0, 0, 1)
 end
 
-function relax_system(mesh)
+function relax_system()
     #We create a simulation with 'SD' driver
-    sim = Sim(mesh; driver="SD", name="skyrmion")
+    sim = Sim(mesh; driver="SD", name="skx")
 
-    #We set mu_s of the system
-    set_mu_s(sim, 1.0)
-    sim.driver.max_tau = 1000.0
-
-    #Set the exchange, dmi and zeeman
-    add_exch(sim, 1.0; name="exch")
-    add_zeeman(sim, (0, 0, 3.75e-3))
-    add_dmi(sim, 0.09; name="dmi")
+    set_mu_s(sim, mu_s_1) # set mu_s of the system
 
     #Initialize the system using the `m0_fun` function
     init_m0(sim, m0_fun)
 
+    J = 50 * k_B
+    add_exch(sim, J; name="exch")
+    add_dmi(sim, 0.5 * J; name="dmi")
+
+    Hz = 0.2 * J / mu_s_1
+    add_zeeman(sim, (0, 0, Hz)) # the unit of Hz is Tesla
+
     #Relax the system
-    relax(sim; max_steps=2000, stopping_dmdt=1e-4, using_time_factor=false)
+    relax(sim; max_steps=2000, stopping_dmdt=0.01)
+
+    #Save the magnetization to vtk file
+    save_vtk(sim, "skx"; fields=["exch", "dmi"])
 
     return sim
 end
 
-sim = relax_system(mesh);
+sim = relax_system();
 
 plot_m(sim)
 
