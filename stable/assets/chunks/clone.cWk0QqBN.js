16b326aca81c396c1578fa4322df98e498800415
Documenter.jl <documenter@juliadocs.github.io>
2025-10-26 14:02:40 +0000

    build based on eab193b

---
+++ stable/assets/chunks/clone.cWk0QqBN.js
@@ -0,0 +1,1 @@
+import{b as r}from"./baseUniq._Xwf5j_c.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ stable/assets/chunks/std4.wxNG-eAG.js
@@ -0,0 +1,1 @@
+const s="/MicroMagnetic.jl/stable/assets/std4.RInrt_W3.gif";export{s as _};
