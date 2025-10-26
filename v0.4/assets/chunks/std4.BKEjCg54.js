14e1401eff79a2b1d95205511553dc853f8feb82
Documenter.jl <documenter@juliadocs.github.io>
2025-10-26 14:02:48 +0000

    build based on eab193b

---
+++ v0.4/assets/chunks/std4.BKEjCg54.js
@@ -0,0 +1,1 @@
+const s="/MicroMagnetic.jl/v0.4/assets/std4.RInrt_W3.gif";export{s as _};
--- versions.js
+++ versions.js
@@ -1,8 +1,9 @@
 var DOC_VERSIONS = [
   "dev",
   "stable",
+  "v0.4",
   "v0.3",
   "v0.3.9",
 ];
-var DOCUMENTER_NEWEST = "0.3.9";
+var DOCUMENTER_NEWEST = "0.4.0";
 var DOCUMENTER_STABLE = "stable";
