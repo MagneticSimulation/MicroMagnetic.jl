dbe402a4d145ad240367780cde46d81e19370453
Documenter.jl <documenter@juliadocs.github.io>
2026-05-30 16:01:27 +0000

    build based on 07efd65

--- stable/assets/chunks/clone.cWk0QqBN.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./baseUniq._Xwf5j_c.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ stable/assets/chunks/std4.DM9rhQwn.js
@@ -0,0 +1,1 @@
+const s="/MicroMagnetic.jl/stable/assets/std4.62HC84zv.mp4";export{s as _};
