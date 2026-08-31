a4de4ba88955890bab4efd681477bd411617399c
Documenter.jl <documenter@juliadocs.github.io>
2026-08-31 11:29:35 +0000

    build based on e3464da

---
+++ stable/assets/chunks/std5.1cM2tHnz.js
@@ -0,0 +1,1 @@
+const s="/MicroMagnetic.jl/stable/assets/std5.DkBgebQY.mp4";export{s as _};
---
+++ stable/assets/chunks/swimlanesDiagram-VR7AAH4N.98osW9Tz.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.De3yYLgJ.js";import{_ as a}from"../app.CRwPil_L.js";import"./chunk-5VM5RSS4.CPu7on2J.js";import"./chunk-XXDRQBXY.DiD4B7Eq.js";import"./chunk-POPQ4Y6H.vAyZQgy6.js";import"./chunk-F27PBJKO.1gBrYaAR.js";import"./channel.B40ve0ch.js";import"./framework.BlzrRReM.js";import"./theme.HB9jPNuQ.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
