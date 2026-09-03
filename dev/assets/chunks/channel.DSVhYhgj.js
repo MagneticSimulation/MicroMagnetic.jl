b18feb9451e7b8c611dddded702cd7a718f3c5a8
Documenter.jl <documenter@juliadocs.github.io>
2026-09-03 05:46:15 +0000

    build based on f560f43

---
+++ dev/assets/chunks/channel.DSVhYhgj.js
@@ -0,0 +1,1 @@
+import{U as a,C as n}from"../app.CrsvtVw0.js";const t=(r,o)=>a.lang.round(n.parse(r)[o]);export{t as c};
--- dev/assets/chunks/std5.C1Ah7cyW.js
+++
@@ -1,1 +0,0 @@
-const s="/MicroMagnetic.jl/dev/assets/std5.CuOq5AgB.mp4";export{s as _};
