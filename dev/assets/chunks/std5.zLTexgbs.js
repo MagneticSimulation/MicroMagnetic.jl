487a11a745b7acf555ec0d325756fc2ab69f8f06
Documenter.jl <documenter@juliadocs.github.io>
2026-09-17 05:43:09 +0000

    build based on cf09f7c

---
+++ dev/assets/chunks/std5.zLTexgbs.js
@@ -0,0 +1,1 @@
+const s="/MicroMagnetic.jl/dev/assets/std5.Dewq_GHF.mp4";export{s as _};
---
+++ dev/assets/chunks/swimlanesDiagram-VR7AAH4N.BnSqIeJF.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.C8LgTk3N.js";import{_ as a}from"../app.B2i_pslX.js";import"./chunk-5VM5RSS4.C7HGZbTK.js";import"./chunk-XXDRQBXY.3o7JtWBC.js";import"./chunk-POPQ4Y6H.CaYuNi5W.js";import"./chunk-F27PBJKO.DE9z35Un.js";import"./channel.CQP6U8Sv.js";import"./framework.gM3-hPsp.js";import"./theme.KEhFu23W.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
